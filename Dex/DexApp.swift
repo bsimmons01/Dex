c02a935bcafe446dcb6100e8bb93d27bc7149456
Brian Simmons <bsimmons01@gmail.com>
2025-03-06 13:11:24 -0700

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Brian Simmons on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
