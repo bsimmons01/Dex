7a1792927a42ceaa314da8559132f3d7a59ddf25
Brian Simmons <bsimmons01@gmail.com>
2025-03-08 07:58:31 -0700

    Add Widget Extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Brian Simmons on 3/8/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
